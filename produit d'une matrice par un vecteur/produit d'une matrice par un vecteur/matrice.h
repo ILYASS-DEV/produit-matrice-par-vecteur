954e9516e7d17d7c6ee7f3b62528a192a2255698
Ilyass Aidi <74270120+ILYASS-DEV@users.noreply.github.com>
2021-02-22 23:38:46 +0100

    Add files via upload

---
+++ produit d'une matrice par un vecteur/produit d'une matrice par un vecteur/matrice.h
@@ -0,0 +1,21 @@
+#pragma once
+#include<iostream>
+#include"vecteur.h"
+
+using namespace std;
+class matrice;
+class vecteur;
+class matrice {
+
+	int m[3][3];
+public:
+	matrice(int tab[][3]);
+	matrice();
+	void affiche();
+
+	
+
+};
+
+//Définition de la fonction indépendante
+vecteur produit(matrice M, vecteur V);
